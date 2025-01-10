2e301667852636100e55cc5cc4ed88288de50295
Everton Ceciliano <evertoncecilainsouza@gmail.com>
2025-01-10 11:16:40 -0300

    fix/button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { Image, Text, TouchableOpacity, View } from 'react-native'
-import { s } from './styles'
+import React from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { s } from './styles';
+
 interface ButtonProps {
-  text: string,
-  img?: any,
-  color?: 'primary' | 'secondary',
-  onPress: () => void,
+  text: string;
+  img?: any;
+  color?: 'primary' | 'secondary';
+  onPress: () => void;
 }
-export function Button({ text, img, onPress , color =  'primary'}: ButtonProps) {
-  const buttonStyle = s[color] || s.primary
- 
+
+export function Button({ text, img, onPress, color = 'primary' }: ButtonProps) {
+  const buttonStyle = s[color] || s.primary;
+  const textStyle = color === 'primary' ? s.primaryText : s.secondaryText;
+
   return (
-    <TouchableOpacity style={[s.baseButton,buttonStyle]} onPress={onPress}>
+    <TouchableOpacity style={[s.baseButton, buttonStyle]} onPress={onPress}>
       <View style={s.buttonContent}>
         {img && <Image style={s.img} source={img} />}
-        <Text style={buttonStyle} >{text}</Text>
-      </View> 
+        <Text style={[s.text, textStyle]}>{text}</Text>
+      </View>
     </TouchableOpacity>
-  )
-  
+  );
 }
--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -2,38 +2,42 @@ import { StyleSheet } from 'react-native';
 import { colors } from '../../styles/colors';
 
 export const s = StyleSheet.create({
-
-buttonContent: {
+  buttonContent: {
     borderRadius: 10,
     flexDirection: 'row',
     alignItems: 'center',
-},
+  },
 
-baseButton:{
+  baseButton: {
     borderRadius: 60,
-    paddingInline:92,
-    paddingBlock: 17,
+    paddingHorizontal: 92,
+    paddingVertical: 17,
     width: 356,
     marginHorizontal: 'auto',
-    marginBlock: 15,
+    marginVertical: 15,
+  },
 
-},
-
-primary: {
-    color: colors.gray.bg,
+  primary: {
     backgroundColor: colors.green.light,
-},
-secondary: {
+  },
+  secondary: {
     backgroundColor: colors.gray.dark,
-    color: colors.green.light,
-},
+  },
 
-img: {
+  primaryText: {
+    color: colors.gray.bg,
+  },
+  secondaryText: {
+    color: colors.green.light,
+  },
+
+  img: {
     width: 22,
     height: 22,
     marginRight: 10,
-},
-text: {
-   
+  },
+
+  text: {
     fontSize: 16,
-}})
+  },
+});
